ca989fbc607d5b2f4b0cce48a7ee569d631f1f82
Ayan485 <107705686+Ayan485@users.noreply.github.com>
2022-06-17 22:05:31 +0530

    Update New file generator.py

--- New file generator.py
+++ New file generator.py
@@ -5,7 +5,7 @@ Name = str(input("Enter file name\n"))
 extension = str(input("Enter extension\n"))
 
 
-filepath = r"C:/Users/Desktop/"+ Name + "." + extension
+filepath = r"Enter the location"+ Name + "." + extension
 wb = openpyxl.Workbook()
 Sheet_name = wb.sheetnames
 wb.save(filepath)
